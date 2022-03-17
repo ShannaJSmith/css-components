a44e0a9c11c6c70b4dcdd917a7cb66c0724e0237
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-17 14:16:46 -0400

    create TOC with Link

--- css-components/src/pages/TableOfContents.jsx
+++ css-components/src/pages/TableOfContents.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import './TableOfContents.css';
 
 const TableOfContents = () => {
-  return <div>Table of Contents</div>;
+  return (
+    <div className="TOC">
+      <h1>Table of Contents </h1>
+      <Link to="/background1">
+        <Button variant="primary">Background 1</Button>
+      </Link>
+      <Link to="/background2">
+        <Button variant="info">Background 2</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default TableOfContents;
