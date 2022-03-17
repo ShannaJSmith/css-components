d10dea01a56134400975f907999d1ad52d9feb71
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-17 14:18:58 -0400

    put home buttons on each page

--- css-components/src/pages/Background1.jsx
+++ css-components/src/pages/Background1.jsx
@@ -1,10 +1,15 @@
 import './Background1.css';
 import DarkModeButton from '../components/DarkModeButton';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 const Background1 = () => {
   return (
     <div className="background1">
       <DarkModeButton />
+      <Link to="/">
+        <Button variant="primary">Home</Button>
+      </Link>
     </div>
   );
 };
--- css-components/src/pages/Background2.jsx
+++ css-components/src/pages/Background2.jsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import './Background2.css';
 
 const Background2 = () => {
-  return <div className="background2">Background2</div>;
+  return (
+    <div className="background2">
+      Background2
+      <Link to="/">
+        <Button variant="primary">Home</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default Background2;
