7914f503792689e0a26c2b62f31a4ce20fc99139
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-21 15:58:35 -0400

    map the panel image and heading instead of hardcoding

--- css-components/src/components/ExpandingCards.jsx
+++ css-components/src/components/ExpandingCards.jsx
@@ -1,7 +1,7 @@
 import './ExpandingCards.css';
 import { panelData } from '../panelData';
 
-console.log('data:', panelData);
+// console.log('data:', panelData);
 
 const ExpandingCards = () => {
   const panels = document.querySelectorAll('.panel');
@@ -21,51 +21,17 @@ const ExpandingCards = () => {
 
   return (
     <div className="container">
-      <div
-        className="panel active"
-        style={{
-          backgroundImage:
-            'url(https://images6.alphacoders.com/629/thumb-1920-629544.jpg)',
-        }}
-      >
-        <h3>Tokyo Ghoul</h3>
-      </div>
-      <div
-        className="panel"
-        style={{
-          backgroundImage:
-            'url(https://images6.alphacoders.com/606/thumb-1920-606263.jpg)',
-        }}
-      >
-        <h3>One Piece</h3>
-      </div>
-      <div
-        className="panel"
-        style={{
-          backgroundImage:
-            'url(https://images5.alphacoders.com/613/thumb-1920-613927.jpg)',
-        }}
-      >
-        <h3>Pokemon</h3>
-      </div>
-      <div
-        className="panel"
-        style={{
-          backgroundImage:
-            'url(https://images6.alphacoders.com/311/thumb-1920-311015.jpg)',
-        }}
-      >
-        <h3>Fairy Tail</h3>
-      </div>
-      <div
-        className="panel"
-        style={{
-          backgroundImage:
-            'url(https://images6.alphacoders.com/656/thumb-1920-656029.png)',
-        }}
-      >
-        <h3>Crossover</h3>
-      </div>
+      {panelData.map((panel) => (
+        <div
+          key={panel.id}
+          className="panel active"
+          style={{
+            backgroundImage: `url(${panel.image})`,
+          }}
+        >
+          <h3>{panel.heading}</h3>
+        </div>
+      ))}
     </div>
   );
 };
