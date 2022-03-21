4583b10752c9c6c6c634f94857282a14d49f17d3
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-21 17:09:17 -0400

    try useState for active class

--- css-components/src/components/ExpandingCards.jsx
+++ css-components/src/components/ExpandingCards.jsx
@@ -1,28 +1,34 @@
 import './ExpandingCards.css';
 import { panelData } from '../panelData';
+import { useState } from 'react';
 
 const ExpandingCards = () => {
-  const panels = document.querySelectorAll('.panel');
+  const [active, setActive] = useState(1);
 
-  panels.forEach((panel) => {
-    panel.addEventListener('click', () => {
-      removeActiveClasses();
-      panel.classList.add('active');
-    });
-  });
+  // const panels = document.querySelectorAll('.panel');
 
-  const removeActiveClasses = () => {
-    panels.forEach((panel) => {
-      panel.classList.remove('active');
-    });
-  };
+  // const handleClick = () => {
+  //   panels.forEach((panel) => {
+  //     panel.addEventListener('click', () => {
+  //       removeActiveClasses();
+  //       panel.classList.add('active');
+  //     });
+  //   });
+
+  //   const removeActiveClasses = () => {
+  //     panels.forEach((panel) => {
+  //       panel.classList.remove('active');
+  //     });
+  //   };
+  // };
 
   return (
     <div className="container">
       {panelData.map((panel) => (
         <div
           key={panel.id}
-          className={`panel ${panel.status}`}
+          onClick={() => setActive('active')}
+          className={`panel ${active === panel.id ? 'active' : ''}`}
           style={{
             backgroundImage: `url(${panel.image})`,
           }}
