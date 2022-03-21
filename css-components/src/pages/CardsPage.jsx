f03e30bee3a1d2fad8e7e4b734521a20cdd4f843
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-21 14:19:44 -0400

    add transition to active h3

--- css-components/src/pages/CardsPage.jsx
+++ css-components/src/pages/CardsPage.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ExpandingCards from '../components/ExpandingCards';
-import './CardsPage.css';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
 const CardsPage = () => {
   return (
-    <div className="card-page">
+    <div>
       <h1>Anime Wallpaper</h1>
       <ExpandingCards />
       <Link to="/">
