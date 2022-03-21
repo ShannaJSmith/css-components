bd482ac2523d9ce5599a6023d70c52d6e8fade58
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-21 13:14:14 -0400

    set css styling on image panels

--- css-components/src/pages/CardsPage.jsx
+++ css-components/src/pages/CardsPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ExpandingCards from '../components/ExpandingCards';
+import './CardsPage.css';
 
 const CardsPage = () => {
-  return <ExpandingCards />;
+  return (
+    <div className="card-page">
+      <ExpandingCards />
+    </div>
+  );
 };
 
 export default CardsPage;
