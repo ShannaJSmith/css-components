e6ed6fa6b9ae162812dd6fb1cce4f146531177c3
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-21 12:58:15 -0400

    create divs with wallpaper and h3 titles

--- css-components/src/components/ExpandingCards.jsx
+++ css-components/src/components/ExpandingCards.jsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import './ExpandingCards.css';
 
 const ExpandingCards = () => {
-  return <div>ExpandingCards</div>;
+  return (
+    <div className="container">
+      <div
+        className="panel active"
+        style={{
+          backgroundImage:
+            'url(https://images6.alphacoders.com/629/thumb-1920-629544.jpg)',
+        }}
+      >
+        <h3>Tokyo Ghoul</h3>
+      </div>
+      <div
+        className="panel"
+        style={{
+          backgroundImage:
+            'url(https://images6.alphacoders.com/606/thumb-1920-606263.jpg)',
+        }}
+      >
+        <h3>One Piece</h3>
+      </div>
+      <div
+        className="panel"
+        style={{
+          backgroundImage:
+            'url(https://images5.alphacoders.com/613/thumb-1920-613927.jpg)',
+        }}
+      >
+        <h3>Pokemon</h3>
+      </div>
+      <div
+        className="panel"
+        style={{
+          backgroundImage:
+            'url(https://images6.alphacoders.com/311/thumb-1920-311015.jpg)',
+        }}
+      >
+        <h3>Fairy Tail</h3>
+      </div>
+      <div
+        className="panel"
+        style={{
+          backgroundImage:
+            'url(https://images6.alphacoders.com/656/thumb-1920-656029.png)',
+        }}
+      >
+        <h3>Crossover</h3>
+      </div>
+    </div>
+  );
 };
 
 export default ExpandingCards;
