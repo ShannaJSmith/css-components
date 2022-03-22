5888a9fe2a0807e6a5048fd9d6787debc239521d
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-22 16:50:09 -0400

    import search icon and setup input and btn

--- css-components/src/components/HiddenSearchForm.jsx
+++ css-components/src/components/HiddenSearchForm.jsx
@@ -1,7 +1,16 @@
 import './HiddenSearchForm.css';
+import { BsSearch } from 'react-icons/bs';
+import { Button } from 'react-bootstrap';
 
 const HiddenSearchForm = () => {
-  return <div>HiddenSearchForm</div>;
+  return (
+    <div className="search">
+      <input type="text" className="input" placeholder="Search..." />
+      <Button variant="dark">
+        <BsSearch />
+      </Button>
+    </div>
+  );
 };
 
 export default HiddenSearchForm;
