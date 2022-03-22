6af7248d6f0fa99f21dadafbce85deac2bdcb137
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-22 17:12:24 -0400

    set animation css for button and overall styling

--- css-components/src/components/HiddenSearchForm.jsx
+++ css-components/src/components/HiddenSearchForm.jsx
@@ -3,10 +3,13 @@ import { BsSearch } from 'react-icons/bs';
 import { Button } from 'react-bootstrap';
 
 const HiddenSearchForm = () => {
+  const search = document.querySelector('.search');
+  const btn = document.querySelector('.search-btn');
+  const input = document.querySelector('.input');
   return (
     <div className="search">
       <input type="text" className="input" placeholder="Search..." />
-      <Button variant="dark">
+      <Button variant="dark" className="search-btn">
         <BsSearch />
       </Button>
     </div>
