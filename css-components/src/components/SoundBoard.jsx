cf95e8acde92966c23017705719f70e36a0c3a48
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-23 13:38:52 -0400

    implement sprite option

--- css-components/src/components/SoundBoard.jsx
+++ css-components/src/components/SoundBoard.jsx
@@ -5,22 +5,46 @@ import tada from '../sounds/tada.mp3';
 // import { useState } from 'react';
 import useSound from 'use-sound';
 
-const sounds = [
-  { clip: toriNoUta, label: 'tori-no-uta' },
-  { clip: TGCF, label: 'TGCF' },
-  { clip: tada, label: 'tada' },
-];
+// const sounds = [
+//   { clip: toriNoUta, label: 'tori-no-uta' },
+//   { clip: TGCF, label: 'TGCF' },
+//   { clip: tada, label: 'tada' },
+// ];
 
 const SoundBoard = () => {
   const [playSound] = useSound(toriNoUta, {
     interrupt: true,
     // controlls the speed of song. Values range from 0.5(slow) - 4(fast)
     playbackRate: 0.5,
+    // play different parts of the same audio file
+    sprite: {
+      one: [0, 2000],
+      two: [2000, 6000],
+      three: [6000, 8000],
+    },
   });
   return (
     <div>
-      <div id="buttons" className="sound-btn" onClick={playSound}>
-        Tori no Uta
+      <div
+        id="buttons"
+        className="sound-btn"
+        onClick={() => playSound({ id: 'one' })}
+      >
+        one
+      </div>
+      <div
+        id="buttons"
+        className="sound-btn"
+        onClick={() => playSound({ id: 'two' })}
+      >
+        two
+      </div>
+      <div
+        id="buttons"
+        className="sound-btn"
+        onClick={() => playSound({ id: 'three' })}
+      >
+        three
       </div>
     </div>
   );
