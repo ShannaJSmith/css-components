3af1cd2a2cb398098f8728c352065937fc4f06b8
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-23 14:29:41 -0400

    implement tracks array and states

--- css-components/src/components/AudioPlayer.jsx
+++ css-components/src/components/AudioPlayer.jsx
@@ -2,15 +2,18 @@ import './AudioPlayer.css';
 import toriNoUta from '../sounds/Tori-no-Uta.mp3';
 import TGCF from '../sounds/TGCF-OST.mp3';
 import tada from '../sounds/tada.mp3';
-// import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-// const sounds = [
-//   { clip: toriNoUta, label: 'tori-no-uta' },
-//   { clip: TGCF, label: 'TGCF' },
-//   { clip: tada, label: 'tada' },
-// ];
+const tracks = [
+  { src: toriNoUta, label: 'tori-no-uta' },
+  { src: TGCF, label: 'TGCF' },
+  { src: tada, label: 'tada' },
+];
 
 const AudioPlayer = () => {
+  const [trackIndex, setTrackIndex] = useState(0);
+  const [trackProgress, setTrackProgress] = useState(0);
+  const [playing, setPlaying] = useState(false);
   return (
     <div style={{ marginTop: '100px' }}>
       <div>Play</div>
