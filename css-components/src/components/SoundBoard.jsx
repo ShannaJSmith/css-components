8f19236a23da3384d8c667ec1ab3f53d5a4b02a9
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-23 14:20:06 -0400

    try method with eventlisteners

--- css-components/src/components/SoundBoard.jsx
+++ css-components/src/components/SoundBoard.jsx
@@ -1,58 +1,41 @@
 import './SoundBoard.css';
 import toriNoUta from '../sounds/Tori-no-Uta.mp3';
-import TGCF from '../sounds/TGCF-OST.mp3';
-import tada from '../sounds/tada.mp3';
-// import { useState } from 'react';
+// import TGCF from '../sounds/TGCF-OST.mp3';
+// import tada from '../sounds/tada.mp3';
+import { useState, useEffect } from 'react';
 
-const sounds = [
-  { clip: toriNoUta, label: 'tori-no-uta' },
-  { clip: TGCF, label: 'TGCF' },
-  { clip: tada, label: 'tada' },
-];
+// const sounds = [
+//   { clip: toriNoUta, label: 'tori-no-uta' },
+//   { clip: TGCF, label: 'TGCF' },
+//   { clip: tada, label: 'tada' },
+// ];
 
 const SoundBoard = () => {
-  // const [play, setPlay] = useState(false);
-  const playMusic = () => {};
-  // window.onload = () => {
-  //   sounds.forEach((sound) => {
-  //     // const btn = document.createElement('button');
-  //     // btn.classList.add('sound-btn');
+  const [audio] = useState(new Audio(toriNoUta));
+  const [playing, setPlaying] = useState(false);
 
-  //     // btn.innerText = sound.label;
+  useEffect(() => {
+    playing ? audio.play() : audio.pause();
+  }, [playing]);
 
-  //     // btn.addEventListener('click', () => {
-  //       // stopMusic();
-
-  //       // document.getElementById(sound).play();
-  //     });
-
-  //     document.getElementById('buttons').appendChild(btn);
-  //   });
-
-  // const stopMusic = () => {
-  //   sounds.forEach((sound) => {
-  //     const bgm = document.getElementById(sound);
-
-  //     bgm.pause();
-  //     // reset current time to 0
-  //     bgm.currentTime = 0;
-  //   });
-  // };
-  // };
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, [audio]);
 
   return (
-    <div className="soundboard">
-      {sounds.map((sound) => {
-        <audio id={sound.label} src={sound.clip}></audio>;
-        <div id="buttons" className="sound-btn" onClick={playMusic}>
-          {sound.label}
-        </div>;
-      })}
-
-      {/* another format it can be written in: */}
-      {/* <audio id="tada">
-        <source src={tada}></source>
-      </audio> */}
+    <div>
+      <div
+        id="buttons"
+        className="sound-btn"
+        onClick={() =>
+          setPlaying(false) ? setPlaying(true) : setPlaying(false)
+        }
+      >
+        {playing ? 'Pause' : 'Play'}
+      </div>
     </div>
   );
 };
