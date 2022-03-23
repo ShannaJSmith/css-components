ff40cc0e10c8a0c54d6f08c378d5b9a0a8325cef
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-23 14:21:09 -0400

    restart

--- css-components/src/components/SoundBoard.jsx
+++ css-components/src/components/SoundBoard.jsx
@@ -1,8 +1,8 @@
 import './SoundBoard.css';
 import toriNoUta from '../sounds/Tori-no-Uta.mp3';
-// import TGCF from '../sounds/TGCF-OST.mp3';
-// import tada from '../sounds/tada.mp3';
-import { useState, useEffect } from 'react';
+import TGCF from '../sounds/TGCF-OST.mp3';
+import tada from '../sounds/tada.mp3';
+// import { useState, useEffect } from 'react';
 
 // const sounds = [
 //   { clip: toriNoUta, label: 'tori-no-uta' },
@@ -11,30 +11,10 @@ import { useState, useEffect } from 'react';
 // ];
 
 const SoundBoard = () => {
-  const [audio] = useState(new Audio(toriNoUta));
-  const [playing, setPlaying] = useState(false);
-
-  useEffect(() => {
-    playing ? audio.play() : audio.pause();
-  }, [playing]);
-
-  useEffect(() => {
-    audio.addEventListener('ended', () => setPlaying(false));
-    return () => {
-      audio.removeEventListener('ended', () => setPlaying(false));
-    };
-  }, [audio]);
-
   return (
     <div>
-      <div
-        id="buttons"
-        className="sound-btn"
-        onClick={() =>
-          setPlaying(false) ? setPlaying(true) : setPlaying(false)
-        }
-      >
-        {playing ? 'Pause' : 'Play'}
+      <div id="buttons" className="sound-btn">
+        Play
       </div>
     </div>
   );
