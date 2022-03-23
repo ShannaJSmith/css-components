1b7ea7c5f46d04c5419527253326235e24afdbaa
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-23 13:26:59 -0400

    install use-sound and implement it on single sound

--- css-components/src/components/SoundBoard.jsx
+++ css-components/src/components/SoundBoard.jsx
@@ -3,6 +3,7 @@ import toriNoUta from '../sounds/Tori-no-Uta.mp3';
 import TGCF from '../sounds/TGCF-OST.mp3';
 import tada from '../sounds/tada.mp3';
 // import { useState } from 'react';
+import useSound from 'use-sound';
 
 const sounds = [
   { clip: toriNoUta, label: 'tori-no-uta' },
@@ -11,48 +12,14 @@ const sounds = [
 ];
 
 const SoundBoard = () => {
-  // const [play, setPlay] = useState(false);
-  const playMusic = () => {};
-  // window.onload = () => {
-  //   sounds.forEach((sound) => {
-  //     // const btn = document.createElement('button');
-  //     // btn.classList.add('sound-btn');
-
-  //     // btn.innerText = sound.label;
-
-  //     // btn.addEventListener('click', () => {
-  //       // stopMusic();
-
-  //       // document.getElementById(sound).play();
-  //     });
-
-  //     document.getElementById('buttons').appendChild(btn);
-  //   });
-
-  // const stopMusic = () => {
-  //   sounds.forEach((sound) => {
-  //     const bgm = document.getElementById(sound);
-
-  //     bgm.pause();
-  //     // reset current time to 0
-  //     bgm.currentTime = 0;
-  //   });
-  // };
-  // };
-
+  const [playSound] = useSound(toriNoUta, {
+    interrupt: true,
+  });
   return (
-    <div className="soundboard">
-      {sounds.map((sound) => {
-        <audio id={sound.label} src={sound.clip}></audio>;
-        <div id="buttons" className="sound-btn" onClick={playMusic}>
-          {sound.label}
-        </div>;
-      })}
-
-      {/* another format it can be written in: */}
-      {/* <audio id="tada">
-        <source src={tada}></source>
-      </audio> */}
+    <div>
+      <div id="buttons" className="sound-btn" onClick={playSound}>
+        Tori no Uta
+      </div>
     </div>
   );
 };
