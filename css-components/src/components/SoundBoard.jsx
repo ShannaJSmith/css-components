a6409af77fa418597c79fa20aedb596472e5d6e4
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-23 15:24:55 -0400

    clear dead code

--- css-components/src/components/SoundBoard.jsx
+++ css-components/src/components/SoundBoard.jsx
@@ -1,15 +1,7 @@
 import './SoundBoard.css';
 import toriNoUta from '../sounds/Tori-no-Uta.mp3';
 import TGCF from '../sounds/TGCF-OST.mp3';
-import tada from '../sounds/tada.mp3';
-// import { useState } from 'react';
 import useSound from 'use-sound';
-
-// const sounds = [
-//   { clip: toriNoUta, label: 'tori-no-uta' },
-//   { clip: TGCF, label: 'TGCF' },
-//   { clip: tada, label: 'tada' },
-// ];
 
 const SoundBoard = () => {
   const [playTori] = useSound(toriNoUta, {
