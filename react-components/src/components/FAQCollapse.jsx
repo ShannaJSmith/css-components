68a5ac3a9cd6b7561b535e2f223bb6b37fe0a089
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-24 11:44:16 -0400

    hard code 5 questions

--- react-components/src/components/FAQCollapse.jsx
+++ react-components/src/components/FAQCollapse.jsx
@@ -8,8 +8,44 @@ const FAQCollapse = () => {
       <h1>Frequently Asked Questions</h1>
       <div className="faq-container">
         <div className="faq active">
-          <h3 className="faq-question">What's the best anime?</h3>
+          <h3 className="faq-question">What's your favourite anime?</h3>
           <p className="faq-answer">Inuyasha of course!</p>
+          <button>
+            <BsChevronDown />
+            <RiCloseFill />
+          </button>
+        </div>
+        <div className="faq active">
+          <h3 className="faq-question">Why is it your favourite anime?</h3>
+          <p className="faq-answer">Probably because of nostalgia.</p>
+          <button>
+            <BsChevronDown />
+            <RiCloseFill />
+          </button>
+        </div>
+        <div className="faq active">
+          <h3 className="faq-question">Who is your favourite character?</h3>
+          <p className="faq-answer">Sesshomaru-sama!</p>
+          <button>
+            <BsChevronDown />
+            <RiCloseFill />
+          </button>
+        </div>
+        <div className="faq active">
+          <h3 className="faq-question">What is your favourite genre?</h3>
+          <p className="faq-answer">
+            Fantasy/Supernatural. And romance is nice.
+          </p>
+          <button>
+            <BsChevronDown />
+            <RiCloseFill />
+          </button>
+        </div>
+        <div className="faq active">
+          <h3 className="faq-question">
+            Is there a country you want to visit?
+          </h3>
+          <p className="faq-answer">Japan. Always Japan.</p>
           <button>
             <BsChevronDown />
             <RiCloseFill />
