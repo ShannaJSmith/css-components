10f1f018a6d8e730cf2e264f48516e1eaa191806
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-24 12:16:00 -0400

    add class to button and remove active from other questions

--- react-components/src/components/FAQCollapse.jsx
+++ react-components/src/components/FAQCollapse.jsx
@@ -10,43 +10,43 @@ const FAQCollapse = () => {
         <div className="faq active">
           <h3 className="faq-question">What's your favourite anime?</h3>
           <p className="faq-answer">Inuyasha of course!</p>
-          <button>
+          <button className="faq-toggle">
             <BsChevronDown />
             <RiCloseFill />
           </button>
         </div>
-        <div className="faq active">
+        <div className="faq">
           <h3 className="faq-question">Why is it your favourite anime?</h3>
           <p className="faq-answer">Probably because of nostalgia.</p>
-          <button>
+          <button className="faq-toggle">
             <BsChevronDown />
             <RiCloseFill />
           </button>
         </div>
-        <div className="faq active">
+        <div className="faq">
           <h3 className="faq-question">Who is your favourite character?</h3>
           <p className="faq-answer">Sesshomaru-sama!</p>
-          <button>
+          <button className="faq-toggle">
             <BsChevronDown />
             <RiCloseFill />
           </button>
         </div>
-        <div className="faq active">
+        <div className="faq">
           <h3 className="faq-question">What is your favourite genre?</h3>
           <p className="faq-answer">
             Fantasy/Supernatural. And romance is nice.
           </p>
-          <button>
+          <button className="faq-toggle">
             <BsChevronDown />
             <RiCloseFill />
           </button>
         </div>
-        <div className="faq active">
+        <div className="faq">
           <h3 className="faq-question">
             Is there a country you want to visit?
           </h3>
           <p className="faq-answer">Japan. Always Japan.</p>
-          <button>
+          <button className="faq-toggle">
             <BsChevronDown />
             <RiCloseFill />
           </button>
