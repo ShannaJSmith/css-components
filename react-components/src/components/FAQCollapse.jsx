e1a62e3c67072b3e1ef3b9578525e2e844147017
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-24 11:32:26 -0400

    setup basic html for FAQ component

--- react-components/src/components/FAQCollapse.jsx
+++ react-components/src/components/FAQCollapse.jsx
@@ -1,7 +1,18 @@
 import './FAQCollapse.css';
 
 const FAQCollapse = () => {
-  return <div>FAQ</div>;
+  return (
+    <div>
+      <h1>Frequently Asked Questions</h1>
+      <div className="faq-container">
+        <div className="faq active">
+          <h3 className="faq-question">What's the best anime?</h3>
+          <p className="faq-answer">Inuyasha of course!</p>
+          <button></button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FAQCollapse;
