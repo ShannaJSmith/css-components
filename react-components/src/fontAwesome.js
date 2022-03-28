4f1b876990556536d40ae96099be8338f2cfab84
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-28 13:16:51 -0400

    import fontawesome and create file to import

---
+++ react-components/src/fontAwesome.js
@@ -0,0 +1,7 @@
+// import the library
+import { library } from '@fortawesome/react-fontawesome';
+
+// import your icons
+import { faComment } from '@fortawesome/fontawesome-svg-core';
+
+library.add(faComment);
