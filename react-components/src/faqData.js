0d2b4a41ecc9e18f0e7c0208441e3daec9e5ca69
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-31 12:17:15 -0400

    remove open key from faqData

--- react-components/src/faqData.js
+++ react-components/src/faqData.js
@@ -4,30 +4,25 @@ export const faqData = [
     question: "What's your favourite anime?",
     answer:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam exercitationem ut culpa nesciunt nihil est quaerat repellat veritatis ullam illo, tenetur labore! Unde nihil magnam animi totam similique rerum veniam cumque consequatur!',
-    open: true,
   },
   {
     id: 2,
     question: 'Why is it your favourite anime?',
     answer: 'Probably because of nostalgia',
-    open: false,
   },
   {
     id: 3,
     question: 'Who is your favourite character?',
     answer: 'Sesshomaru-sama!',
-    open: false,
   },
   {
     id: 4,
     question: 'What is your favourite genre?',
     answer: 'Fantasy/Supernatural and romance',
-    open: false,
   },
   {
     id: 5,
     question: 'Is there a country you want to visit?',
     answer: 'Japan. Always Japan.',
-    open: false,
   },
 ];
