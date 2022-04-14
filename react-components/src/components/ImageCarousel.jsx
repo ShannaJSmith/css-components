015b2cec9c93765b194caefa41115414ffe6b0cf
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-04-14 14:45:02 -0400

    setup html for carousel component

--- react-components/src/components/ImageCarousel.jsx
+++ react-components/src/components/ImageCarousel.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import './ImageCarousel.scss';
 const ImageCarousel = () => {
-  return <div>ImageCarousel</div>;
+  return (
+    <div className="carousel">
+      <div className="image-container" id="imgs">
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+      <div className="buttons-container"></div>
+    </div>
+  );
 };
 
 export default ImageCarousel;
